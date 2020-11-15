a3a01620d288018fe2b4e47cdd0cd4c78052eb4a
ribt <ribt115@gmail.com>
2020-11-15 19:44:24 +0100

    renaming gameplay.c -> game.c

--- game.c
+++ game.c
@@ -1,33 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "board.h"
 #include "display.h"
-
-/**
- * @file game.c
- * @brief a simple Gygès game
- * @author ribt
- */
-int main(){
-    board game = new_game();
-
-    place_piece(game, THREE, SOUTH_P, 0);
-    place_piece(game, ONE, SOUTH_P, 1);
-    place_piece(game, TWO, SOUTH_P, 2);
-    place_piece(game, THREE, SOUTH_P, 3);
-    place_piece(game, ONE, SOUTH_P, 4);
-    place_piece(game, TWO, SOUTH_P, 5);
-
-    place_piece(game, THREE, NORTH_P, 0);
-    place_piece(game, ONE, NORTH_P, 1);
-    place_piece(game, TWO, NORTH_P, 2);
-    place_piece(game, THREE, NORTH_P, 3);
-    place_piece(game, ONE, NORTH_P, 4);
-    place_piece(game, TWO, NORTH_P, 5);
-    
-
-    disp_board(game);
-  
-    return 0;
+#define DIMENSION 6
+#define NB_SIZE 3
+#define NB_PLAYERS 2
+#define NB_INITIAL_PIECES 2
+void firstphase(board game){
+	int pawn = -1;
+	int column = -1;
+	for(int i = 0; i < 6; i++){
+		while(1){
+			printf("C'est au joueur Sud de jouer. Quel pion voulez vous jouer ?\n1/2/3 :");
+			scanf("%d", &pawn);
+			if(nb_pieces_available(game, pawn, SOUTH_P) > 0){break;};
+			printf("La pion n'est pas disponible\n");
+		}
+		printf("Il vous reste %d pion(s) de %d.\n",nb_pieces_available(game, pawn, SOUTH_P), pawn);
+		while(1){
+			printf("Sur quelle colonne voulez vous jouer ?\n");
+			scanf("%d", &column);
+			if(place_piece(game, pawn, SOUTH_P, column - 1) == OK){break;};
+			printf("La case est déjà occupé ou n'existe pas.\n");
+		}
+		printf("Le pion de %d a été posé en (%d,1).\n", pawn, column);
+		next_player(SOUTH_P);
+		disp_board(game);
+		while(1){
+			printf("C'est au joueur Nord de jouer. Quel pion voulez vous jouer ?\n1/2/3 :");
+			scanf("%d", &pawn);
+			if(nb_pieces_available(game, pawn, NORTH_P) > 0){break;};
+			printf("La pion n'est pas disponible\n");
+		}
+		printf("Il vous reste %d pion(s) de %d.\n",nb_pieces_available(game, pawn, NORTH_P), pawn);
+		while(1){
+			printf("Sur quelle colonne voulez vous jouer ?\n");
+			scanf("%d", &column);
+			if(place_piece(game, pawn, NORTH_P, column - 1) == OK){break;};
+			printf("La case est déjà occupé ou n'existe pas.\n");
+		}
+		printf("Le pion de %d a été posé en (%d,6).\n", pawn, column);
+		next_player(NORTH_P);
+		disp_board(game);
+	}
 }
 
-
+int main(void){
+	board game = new_game();//To initialize the board
+	printf("Le plateau a été correctement initialisé.\n");
+	firstphase(game);
+	printf("Fin du placement des premiers pions, début du jeu\n");
+	destroy_game(game);
+	return 0;
+}
