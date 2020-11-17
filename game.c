803a399e44e30017f47f9704cfcf7238a8f032d8
Suake <delagonimuss@gmail.com>
2020-11-17 16:30:31 +0100

    Fixed a small bug

--- game.c
+++ game.c
@@ -3,7 +3,7 @@
 #include "board.h"
 #include "display.h"
 
-int initializetable(int table[],int size, int value){
+void initializetable(int table[],int size, int value){
 	for(int i = 0; i < size; i++){
 		table[i] = value;
 	}
@@ -16,11 +16,11 @@ void firstphase(board game){
 		initializetable(allpawn, NB_SIZE, NB_INITIAL_PIECES);
 		initializetable(historymoves, DIMENSION, 0);
 		int pawn_size = 0;
-		if(k == 0){printf("Joueur Sud, veuillez saisir de gauche à droite le placement de vos pions\n");
-		}else{printf("Joueur Nord, veuillez saisir de gauche à droite le placement de vos pions\n");}
-		for(int i = 0; i < DIMENSION; i++){
+		if(k == 0){printf("Joueur Sud, veuillez saisir un par un, de gauche à droite le placement de vos pions puis valider avec entrée.\n");
+		}else{printf("Joueur Nord, veuillez saisir un par un,de gauche à droite le placement de vos pions puis valider avec entrée.\n");}
+		for(int i = 0; i < DIMENSION; i++){	//To place as many pawns as there are columns
 			scanf("%d", &pawn_size);
-			while (allpawn[pawn_size-1] == 0 || pawn_size > 4 || pawn_size < 1){	//Check if the pawn is available.
+			while (allpawn[pawn_size-1] == 0 || pawn_size > 3 || pawn_size < 1){	//Check if the pawn is available.
 				printf("\033[H\033[2J");	//clear output screen 
 				if(pawn_size > 0 && pawn_size < 4){printf("Il ne vous reste plus de pion de %d.\n", pawn_size);
 				}else{printf("Ce pion n'existe pas.\n");}
@@ -41,7 +41,6 @@ void firstphase(board game){
 		}
 		printf("\033[H\033[2J");	//clear output screen 
 	}
-	disp_board(game);	//Displays the board
 }
 
 
@@ -51,6 +50,7 @@ int main(void){
 	printf("Le plateau a été correctement initialisé.\n");
 	firstphase(game);
 	printf("Fin du placement des premiers pions, début du jeu\n");
+	disp_board(game);	//Displays the board
 	destroy_game(game);	
 	return 0;	//Deletes the board
 }
