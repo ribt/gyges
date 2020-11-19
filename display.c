223f4f80e8248818ff5a52dd20693c0027b168c8
ribt <ribt115@gmail.com>
2020-11-19 11:56:49 +0100

    better display

--- display.c
+++ display.c
@@ -2,28 +2,28 @@
 #include "board.h"
 
 void disp_board(board game) {
-    printf("               \033[0;90mNord\n       ┌───┬───┬\033[1;0m o \033[0;90m┬───┬───┐\n");
-    printf("       │   │   │   │   │   │\n");
+    printf("               \033[0;90mNord\n       ╔═══╦═══╦═\033[1;93m⬤\033[0;90m═╦═══╦═══╗\n     ┌─╨─┬─╨─┬─╨─┬─╨─┬─╨─┬─╨─┐\n");
     for (int line = DIMENSION-1; line >= 0; line--) {
-        printf("      ");
+        printf("     ");
         for (int column = 0; column < DIMENSION; column++) {
-            printf("\033[0m ");
+            printf("\033[0;90m│ ");
             switch (get_piece_size(game, line, column)) {
-                case NONE: printf("o"); break;
-                case ONE: printf("\033[1;34m1"); break;
-                case TWO: printf("\033[1;33m2"); break;
-                case THREE: printf("\033[1;31m3"); break;
+                case NONE: printf("  "); break;
+                case ONE: printf("\033[1;34m1 "); break;
+                case TWO: printf("\033[1;33m2 "); break;
+                case THREE: printf("\033[1;31m3 "); break;
             }
-            if (column < DIMENSION-1) {
-                printf(" \033[0;90m─\033[0m");
+            if (column == DIMENSION-1) {
+                printf("\033[0;90m│");
             }    
         }
         printf("\n");
         if (line > 0) {
-            printf("      \033[0;90m |   |   |   |   |   |\n");
+            printf("    \033[0;90m ├───┼───┼───┼───┼───┼───┤\n");
         }
     }
-    printf("      \033[0;90m │   │   │   │   │   │\n       └───┴───┴\033[0m o \033[0;90m┴───┴───┘\n                Sud\033[0m\n");
+    //printf("     \033[0;90m└─╥─┴─╥─┴──╥┴╥──┴─╥─┴─╥─┘\n       ║   ║   ┌╨─╨┐   ║   ║\n       ╚═══╩═══╡   ╞═══╩═══╝\n               └───┘\n                Sud\033[0m\n");
+    printf("     \033[0;90m└─╥─┴─╥─┴─╥─┴─╥─┴─╥─┴─╥─┘\n       ╚═══╩═══╩═\033[1;93m⬤\033[0;90m═╩═══╩═══╝\n                Sud\033[0m\n");
 }
 
 void disp_error(char * message) {
