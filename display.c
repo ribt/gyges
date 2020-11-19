e0e6f2cc0500ec256aa112440fd0e23f2cc00dfc
ribt <ribt115@gmail.com>
2020-11-19 13:41:58 +0100

    explain shell colors

--- display.c
+++ display.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include "board.h"
 
+#define clear_screen() printf("\033[H\033[2J")
+
 void disp_board(board game) {
     printf("               \033[0;90mNord\n       ╔═══╦═══╦═\033[1;93m⬤\033[0;90m═╦═══╦═══╗\n     ┌─╨─┬─╨─┬─╨─┬─╨─┬─╨─┬─╨─┐\n");
+        // \033[0;90m -> grey         \033[1;93m -> light yellow and bold  
     for (int line = DIMENSION-1; line >= 0; line--) {
         printf("     ");
         for (int column = 0; column < DIMENSION; column++) {
             printf("\033[0;90m│ ");
             switch (get_piece_size(game, line, column)) {
                 case NONE: printf("  "); break;
-                case ONE: printf("\033[1;34m1 "); break;
-                case TWO: printf("\033[1;33m2 "); break;
-                case THREE: printf("\033[1;31m3 "); break;
+                case ONE: printf("\033[1;34m1 "); break;   // blue and bold
+                case TWO: printf("\033[1;33m2 "); break;   // yellow and bold
+                case THREE: printf("\033[1;31m3 "); break; // red and bold
             }
             if (column == DIMENSION-1) {
                 printf("\033[0;90m│");
@@ -22,12 +25,15 @@ void disp_board(board game) {
             printf("    \033[0;90m ├───┼───┼───┼───┼───┼───┤\n");
         }
     }
-    //printf("     \033[0;90m└─╥─┴─╥─┴──╥┴╥──┴─╥─┴─╥─┘\n       ║   ║   ┌╨─╨┐   ║   ║\n       ╚═══╩═══╡   ╞═══╩═══╝\n               └───┘\n                Sud\033[0m\n");
     printf("     \033[0;90m└─╥─┴─╥─┴─╥─┴─╥─┴─╥─┴─╥─┘\n       ╚═══╩═══╩═\033[1;93m⬤\033[0;90m═╩═══╩═══╝\n                Sud\033[0m\n");
+                                                                                              // \033[0m -> regular text after this print
 }
 
 void disp_error(char * message) {
     printf("\033[101mERREUR\033[49m\n\033[1;31m%s\n\033[0m", message);
+    // \033[101m -> red background for ERREUR
+    // \033[1;31m -> red and bold for the message
+    // \033[0m -> regular text after this print
 }
 
 void disp_player_name(player this_player) {
--- display.h
+++ display.h
@@ -2,3 +2,5 @@
 
 void disp_board(board game);
 void disp_error(char * message);
+void disp_player_name(player this_player);
+return_code disp_movement(board game, player current_player, int line, int column, direction directions[], int nbr_directions);
