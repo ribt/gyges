e8cb01bf22960dee0e3a68dddf14349e6d001546
ribt <ribt115@gmail.com>
2020-12-09 15:09:04 +0100

    new way to test our code

--- board/test.c
+++ board/test.c
@@ -13,21 +13,38 @@ void affichage(board game) {
 int main() {
     board game = new_game();
 
-    place_piece(game, ONE, SOUTH_P, 0);
-    place_piece(game, THREE, SOUTH_P, 1);
-    place_piece(game, TWO, SOUTH_P, 2);
-    place_piece(game, THREE, SOUTH_P, 3);
-    place_piece(game, ONE, SOUTH_P, 4);
-    place_piece(game, TWO, SOUTH_P, 5);
+    printf("%d\n", place_piece(game, ONE, SOUTH_P, 0));
+    printf("%d\n", place_piece(game, THREE, SOUTH_P, 1));
+    printf("%d\n", place_piece(game, TWO, SOUTH_P, 2));
+    printf("%d\n", place_piece(game, THREE, SOUTH_P, 3));
+    printf("%d\n", place_piece(game, ONE, SOUTH_P, 4));
+    printf("%d\n", place_piece(game, TWO, SOUTH_P, 5));
 
-    place_piece(game, ONE, NORTH_P, 0);
-    place_piece(game, ONE, NORTH_P, 1);
-    place_piece(game, TWO, NORTH_P, 2);
-    place_piece(game, TWO, NORTH_P, 3);
-    place_piece(game, THREE, NORTH_P, 4);
-    place_piece(game, THREE, NORTH_P, 5);
+    printf("%d\n", place_piece(game, ONE, NORTH_P, 0));
+    printf("%d\n", place_piece(game, ONE, NORTH_P, 1));
+    printf("%d\n", place_piece(game, TWO, NORTH_P, 2));
+    printf("%d\n", place_piece(game, TWO, NORTH_P, 3));
+    printf("%d\n", place_piece(game, THREE, NORTH_P, 4));
+
+    printf("%d\n", place_piece(game, THREE, NORTH_P, 0));
+    printf("%d\n", place_piece(game, THREE, NORTH_P, 6));
+    printf("%d\n", place_piece(game, THREE, NORTH_P, -1));
+
+    printf("%d\n", place_piece(game, ONE, NORTH_P, 5));
+    printf("%d\n", place_piece(game, TWO, NORTH_P, 5));
+    printf("%d\n", place_piece(game, NONE, NORTH_P, 5));
+    printf("%d\n", place_piece(game, THREE+1, NORTH_P, 5));
+
+    printf("%d\n", place_piece(game, THREE, SOUTH_P, 5));
+    printf("%d\n", place_piece(game, THREE, -1, 5));
+    printf("%d\n", place_piece(game, THREE, 0, 5));
+    printf("%d\n", place_piece(game, THREE, 3, 5));
+
+    printf("%d\n", place_piece(game, THREE, NORTH_P, 5));
 
     affichage(game);
+
+    printf("%d\n", movement_left(game));
 
     return 0;
 }
